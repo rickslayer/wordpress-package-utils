370f191e9143ddd4521fa789d99393a9aaa9d09e
rickslayer <paulo@actio.net.br>
2019-07-15 10:38:26 -0300

    dbhandler

--- src/Handler/DatabaseHandler.php
+++ src/Handler/DatabaseHandler.php
@@ -1,1 +1,15 @@
+<?php
+namespace Actio\Handler;
 
+class DBHandler {
+    private $query;
+    private $id;
+    private $instance;
+
+    public function __construct()
+    {
+        global $wpdb;
+        $this->instance = $wpdb;
+    }
+
+}
